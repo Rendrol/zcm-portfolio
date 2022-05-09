e9110f9246c47c7a08a61f48ad007121026363c8
RenZhuo <rendrol@nas-10-241-36-49.york.ac.uk>
2022-05-09 16:09:46 +0100

    modified: change file names of the first project "she glanced at me"; adjust the animatation effects on the hover items

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ $(".hover-item-title").hover(function() {
   //change image due to the hover item
   var imgUrl = $(this).data("src");
   $("#sticky-image").attr("src", imgUrl);
-  //image animation - fade in
+  //image animation - hover:fade in
   $("#sticky-image").stop();
-  $("#sticky-image").fadeIn();
-  //image animation - zoom in
-  $("#sticky-image").css("transform","scale("+ 1.1 + ")")
+  $("#sticky-image").fadeIn(1000);
+  //image animation - hover:zoom in
+  $(".hover-base-sticky").css("transform","scale("+ 1.05 +")")
 }, function() {
+  //image animation - hoverout:fade out
   $("#sticky-image").stop();
-  $("#sticky-image").fadeOut();
+  $(".hover-base-sticky").css("transform","scale("+ 1 +")");
+  $("#sticky-image").fadeOut(1000);
+  //image animation - hoverout:zoom out
 })
-
-//#sticky-image:hover{
-//   transform: scale(1.5);
-// }
