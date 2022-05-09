f0b60cddf11674854ac42642a6c910e3be8f56e1
RenZhuo <rendrol@nas-10-241-36-49.york.ac.uk>
2022-05-10 00:02:22 +0100

    navbar styling, nav bar scroll down animation

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+//scroll down navbar animation
+$(function(){
+  var scroll = $(document).scrollTop();
+  var navHeight = $(".navbar-container").outerHeight();
+
+  $(window).scroll(function(){
+    var scrolled = $(document).scrollTop();
+
+    if (scrolled > navHeight) {
+      $(".navbar-container").addClass("animate");
+    } else{
+      $(".navbar-container").removeClass("animate");
+    }
+
+    if (scrolled > scroll){
+      $(".navbar-container").removeClass("sticky");
+    } else{
+      $(".navbar-container").addClass("sticky");
+    }
+
+    scroll = $(document).scrollTop();
+
+  });
+})
+
+
 // hover
 $(".hover-item-title").hover(function() {
   //change image due to the hover item
@@ -9,9 +35,9 @@ $(".hover-item-title").hover(function() {
   //image animation - hover:zoom in
   $(".hover-base-sticky").css("transform","scale("+ 1.05 +")")
 }, function() {
-  //image animation - hoverout:fade out
   $("#sticky-image").stop();
-  $(".hover-base-sticky").css("transform","scale("+ 1 +")");
-  $("#sticky-image").fadeOut(1000);
   //image animation - hoverout:zoom out
+  $(".hover-base-sticky").css("transform","scale("+ 1 +")");
+  //image animation - hoverout:fade out
+  $("#sticky-image").fadeOut(1000);
 })
