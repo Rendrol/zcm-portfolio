f168cf999781ec31bad4ae8c4eec1fa412f7832a
RenZhuo <rendrol@nas-10-241-36-49.york.ac.uk>
2022-05-10 14:48:30 +0100

    subpage: background image

---
+++ subpage.js
@@ -0,0 +1,24 @@
+//scroll down navbar animation
+$(function(){
+  var scroll = $(document).scrollTop();
+  var navHeight = $(".navbar-container").outerHeight();
+
+  $(window).scroll(function(){
+    var scrolled = $(document).scrollTop();
+
+    if (scrolled > navHeight) {
+      $(".navbar-container").addClass("animate");
+    } else{
+      $(".navbar-container").removeClass("animate");
+    }
+
+    if (scrolled > scroll){
+      $(".navbar-container").removeClass("sticky");
+    } else{
+      $(".navbar-container").addClass("sticky");
+    }
+
+    scroll = $(document).scrollTop();
+
+  });
+})
